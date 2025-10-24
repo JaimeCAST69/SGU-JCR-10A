75cf5d0759f765ecd2a8e72089183d6711635be0
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-10-24 13:38:24 -0600

    [UPDATE] API funcionando

--- server/src/main/java/sgu/deployment/server/modules/user/UserController.java
+++ server/src/main/java/sgu/deployment/server/modules/user/UserController.java
@@ -1,13 +1,47 @@
 package sgu.deployment.server.modules.user;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sgu.deployment.server.utils.APIResponse;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    @Autowired
 
+    @Autowired
+    private UserService clientService;
+
+    @GetMapping("")
+    public ResponseEntity<APIResponse> findAll() {
+        APIResponse response = clientService.findAll();
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<APIResponse> saveClient(@Valid @RequestBody User payload) {
+        APIResponse response = clientService.saveUser(payload);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse> findById(@PathVariable("id") Long id) {
+        APIResponse response = clientService.findById(id);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @PutMapping("")
+    public ResponseEntity<APIResponse> updateClient(@RequestBody User payload) {
+        APIResponse response = clientService.updateUser(payload);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<APIResponse> deleteClient(@RequestBody User payload) {
+        APIResponse response = clientService.deleteUser(payload);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
 }
